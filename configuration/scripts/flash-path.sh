43c1634fe8308b83159ddce2bc5b5ee15226d5b6
Mikkel Schmidt <mikkel.schmidt@gmail.com>
2024-12-28 12:54:57 +0000

    Deployed  commit.
    https://github.com/Rat-OS/RatOS-configurator/compare/...ba5ebd7f472695ed3e238e9894972961921b351d

--- configuration/scripts/flash-path.sh
+++ configuration/scripts/flash-path.sh
@@ -15,38 +15,20 @@ if [ "$MCU" == "" ]; then
 fi
 pushd "${KLIPPER_DIR}" || exit
 service klipper stop
-if
-    dfuDevicesPreFlash=$(lsusb | grep -c "0483:df11"); then
-    if [ -h "$MCU" ]; then
-        echo "Flashing $MCU"
-        sudo make flash FLASH_DEVICE="$MCU"
-    fi
-else
-    dfuDevicesPreFlash=$(lsusb | grep -c "2e8a:0003"); then
-    if [ -h "$MCU" ]; then
-        echo "Flashing $MCU"
-        sudo make flash FLASH_DEVICE="$MCU"
-    fi
+dfuDevicesPreFlash=$(lsusb | grep -c "0483:df11")
+if [ -h "$MCU" ]; then
+    echo "Flashing $MCU"
+    sudo make flash FLASH_DEVICE="$MCU"
 fi
 sleep 5
 retVal=1
 if [ -h "$MCU" ]; then
 	retVal=0
 else
-	dfuDevicesPostFlash=$(lsusb | grep -c "0483:df11"); then 
+	dfuDevicesPostFlash=$(lsusb | grep -c "0483:df11")
 	if [ "$dfuDevicesPreFlash" -eq 0 ] && [ "$dfuDevicesPostFlash" -eq 1 ]; then
 		echo "Seems like flashing failed, but the device is still in DFU mode. Attempting to recover."
 		make flash FLASH_DEVICE=0483:df11
-		sleep 5
-		if [ -h "$MCU" ]; then
-			retVal=0
-		fi
-	fi
-else
-    dfuDevicesPostFlash=$(lsusb | grep -c "2e8a:0003"); then
-	if [ "$dfuDevicesPreFlash" -eq 0 ] && [ "$dfuDevicesPostFlash" -eq 1 ]; then
-		echo "Seems like flashing failed, but the device is still in DFU mode. Attempting to recover."
-		make flash FLASH_DEVICE=2e8a:0003
 		sleep 5
 		if [ -h "$MCU" ]; then
 			retVal=0
