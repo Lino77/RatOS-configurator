d8312ea11c71a3c85dbc50e9e728dc529ed5a02f
Mikkel Schmidt <mikkel.schmidt@gmail.com>
2024-12-28 12:34:00 +0000

    Deployed  commit.
    https://github.com/Rat-OS/RatOS-configurator/compare/...ba5ebd7f472695ed3e238e9894972961921b351d

--- app/scripts/flash-path.sh
+++ app/scripts/flash-path.sh
@@ -1,3 +1,4 @@
+
 #!/bin/bash
 if [ "$EUID" -ne 0 ]
   then echo "ERROR: Please run as root"
@@ -16,44 +17,26 @@ if [ "$MCU" == "" ]; then
 fi
 pushd "${KLIPPER_DIR}" || exit
 service klipper stop
+dfuDevicesPreFlash=$(lsusb | grep -c "0483:df11")
 
-if dfuDevicesPreFlash=$(lsusb | grep -c "0483:df11"); then
-
-	if [ -h "$FLASH_PATH" ]; then
-		echo "Flashing $MCU at $FLASH_PATH"
-		make flash FLASH_DEVICE="$FLASH_PATH"
-	else
-	echo "$MCU does not seems to be connected, at least it was not found at $FLASH_PATH"
-	exit 1;
-	fi
-else dfuDevicesPreFlash=$(lsusb | grep -c "2e8a:0003"); then
-
-	if [ -h "$FLASH_PATH" ]; then
-		echo "Flashing $MCU at $FLASH_PATH"
-		make flash FLASH_DEVICE="$FLASH_PATH"
-	else
-	echo "$MCU does not seems to be connected, at least it was not found at $FLASH_PATH"
-	exit 1;
-	fi
+if [ -h "$FLASH_PATH" ]; then
+    echo "Flashing $MCU at $FLASH_PATH"
+    make flash FLASH_DEVICE="$FLASH_PATH"
+else
+  echo "$MCU does not seems to be connected, at least it was not found at $FLASH_PATH"
+  exit 1;
 fi
 sleep 5
+
 retVal=1
 
 if [ -h "$MCU" ]; then
 	retVal=0
-else dfuDevicesPostFlash=$(lsusb | grep -c "0483:df11"); then
+else
+	dfuDevicesPostFlash=$(lsusb | grep -c "0483:df11")
 	if [ "$dfuDevicesPreFlash" -eq 0 ] && [ "$dfuDevicesPostFlash" -eq 1 ]; then
 		echo "Seems like flashing failed, but the device is still in DFU mode. Attempting to recover."
 		make flash FLASH_DEVICE=0483:df11
-		sleep 5
-		if [ -h "$MCU" ]; then
-			retVal=0
-		fi
-	fi
-else dfuDevicesPostFlash=$(lsusb | grep -c "2e8a:0003"); then
-	if [ "$dfuDevicesPreFlash" -eq 0 ] && [ "$dfuDevicesPostFlash" -eq 1 ]; then
-		echo "Seems like flashing failed, but the device is still in DFU mode. Attempting to recover."
-		make flash FLASH_DEVICE=2e8a:0003
 		sleep 5
 		if [ -h "$MCU" ]; then
 			retVal=0
