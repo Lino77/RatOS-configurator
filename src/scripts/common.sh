cac8e66966e32ab9fcc87fb8a35f0dbefe2705e2
Lino77 <lino.nardandrea@gmail.com>
2026-05-10 08:12:14 +0200

    w

--- src/scripts/common.sh
+++ src/scripts/common.sh
@@ -62,11 +62,9 @@ pnpm_install() {
 			report_status "Deleting root owned node_modules"
 			rm -rf "$SRC_DIR/node_modules"
 		fi
-		sudo -u "${RATOS_USERNAME}" pnpm config set --location=project allowScripts true
-        sudo -u "${RATOS_USERNAME}" pnpm install --no-ignore-scripts --aggregate-output --no-color --config.confirmModulesPurge=false
+        sudo -u "${RATOS_USERNAME}" pnpm install --frozen-lockfile --aggregate-output --no-color --config.confirmModulesPurge=false
     else
-	    pnpm config set --location=project allowScripts true
-		pnpm install --no-ignore-scripts --aggregate-output --no-color --config.confirmModulesPurge=false
+		pnpm install --frozen-lockfile --aggregate-output --no-color --config.confirmModulesPurge=false
 	fi
     popd || exit 1
 }
