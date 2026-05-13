1b8d24bd5f78a47e00f314f859a28eb5530275b8
Lino77 <lino.nardandrea@gmail.com>
2026-05-13 07:33:59 +0200

    u5

--- src/scripts/common.sh
+++ src/scripts/common.sh
@@ -62,11 +62,9 @@ pnpm_install() {
 			report_status "Deleting root owned node_modules"
 			rm -rf "$SRC_DIR/node_modules"
 		fi
-		sudo -u "${RATOS_USERNAME}" pnpm config set only-built-dependencies esbuild
-        sudo -u "${RATOS_USERNAME}" pnpm install --aggregate-output --no-color --config.confirmModulesPurge=false
+		sudo -u "${RATOS_USERNAME}" pnpm install --aggregate-output --no-color --config.confirmModulesPurge=false --only-built-dependencies=esbuild
     else
-		pnpm config set only-built-dependencies esbuild
-		pnpm install --aggregate-output --no-color --config.confirmModulesPurge=false
+		pnpm install --aggregate-output --no-color --config.confirmModulesPurge=false --only-built-dependencies=esbuild
 	fi
     popd || exit 1
 }
